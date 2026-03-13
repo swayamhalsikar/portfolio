3db13014e4dce2abd721aec94fad7993d626e86e
Swayam Halsikar <swayamhalsikar0@gmail.com>
2026-03-13 22:32:38 +0530

    viewport fix for mobile

--- script.js
+++ script.js
@@ -64,3 +64,7 @@ let scrolled = (scrollTop / height) * 100;
 document.getElementById("progress-bar").style.width = scrolled + "%";
 
 };
+
+window.addEventListener("resize", () => {
+document.body.style.width = window.innerWidth + "px";
+});
